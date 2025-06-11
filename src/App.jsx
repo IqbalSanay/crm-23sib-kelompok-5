c6fa6186a414ae751a9ddd55f2a604a0912841ba
denana19 <frisdenagustin1908@gmail.com>
2025-06-11 16:11:07 +0700

    MA pt 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { Routes, Route } from "react-router-dom"
-import MainLayout from "./components/MainLayout"
-import Dashboard from "./pages/Dashboard"
-import CustomerManagement from "./pages/CustomerManagement"
-// import SalesManagement from "./pages/Penjualan"
-import Penjualan from "./pages/Penjualan"
-import Tracking from "./pages/Tracking"
-import OrderForm from "./pages/OrderForm"
-import Feedback from "./pages/Feedback"
-import Helpdesk from "./pages/Helpdesk"
-import Pelanggan from "./pages/Pelanggan"
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import AuthLayout from "./components/AuthLayout";  // Layout baru
+import Dashboard from "./pages/Dashboard";
+import CustomerManagement from "./pages/CustomerManagement";
+import Penjualan from "./pages/Penjualan";
+import Tracking from "./pages/Tracking";
+import OrderForm from "./pages/OrderForm";
+import Feedback from "./pages/Feedback";
+import Helpdesk from "./pages/Helpdesk";
+import Pelanggan from "./pages/Pelanggan";
+import SignInPage from "./pages/SignInPage";
+import CampaignPage from "./pages/CampaignPage";
 
 function App() {
-
   return (
-
     <Routes>
+      {/* Route khusus Auth (SignInPage) */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<SignInPage />} />
+      </Route>
+
+      {/* Route utama pakai MainLayout */}
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        {/* <Route path="/Pelanggan" element={<CustomerManagement />} /> */}
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/CampaignPage" element= {<CampaignPage/>} />
         <Route path="/Penjualan" element={<Penjualan />} />
         <Route path="/Tracking" element={<Tracking />} />
         <Route path="/OrderForm" element={<OrderForm />} />
-        <Route path="/pelanggan" element={<CustomerManagement />} />
+        <Route path="/pelanggan" element={<Pelanggan />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/helpdesk" element={<Helpdesk />} />
-        <Route path="/Pelanggan" element={<Pelanggan />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
